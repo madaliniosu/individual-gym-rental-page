7ccc78b192be70654f0e37393063653a74a2ceb7
105gm <iosu.gm@gmail.com>
2026-04-15 15:54:09 +0200

    "Added dummy data to pages"

---
+++ app/(app)/book/page.tsx
@@ -0,0 +1,7 @@
+export default function BookPage() {
+  return (
+    <main>
+      <h1>Book</h1>
+    </main>
+  )
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <main>
+      <h1>Signup</h1>
+    </main>
+  )
+}
